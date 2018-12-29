0f367105ac8db94bfd68a032897bb57e68323bc1
Prakasa <prakasa@devetek.com>
2018-12-29 21:55:58 +0700

    remove build file on dev environment

--- src/app/config/index.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
-var app_1 = require('@app/config/app');
-exports.app = app_1.default;
-//# sourceMappingURL=index.js.map
--- src/helpers/DOM/index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
-var loadCss_1 = require('@helpers/DOM/loadCss');
-exports.loadCss = loadCss_1.default;
-var canUseDOM_1 = require('@helpers/DOM/canUseDOM');
-exports.canUseDOM = canUseDOM_1.default;
-//# sourceMappingURL=index.js.map
